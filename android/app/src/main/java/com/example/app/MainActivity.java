d384795fe8869f1d62b0eba31e265be0a89424ec
ElenMuradyan <muradyanelen0701@gmail.com>
2025-01-05 02:26:02 +0400

    import AppCompatActivity

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,20 @@
 package com.example.app;
 
-import com.getcapacitor.BridgeActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.admob.AdMob; 
+import com.getcapacitor.community.admob.AdMobConfig; 
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AdMobConfig config = new AdMobConfig();
+        config.setAppId("ca-app-pub-4052131431424102~5673792257");
+        AdMob.initialize(config);
+    }
+}
