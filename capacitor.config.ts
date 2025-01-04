f8f24cf6466b700df0cd5f7e0f0ab39293b9780a
ElenMuradyan <muradyanelen0701@gmail.com>
2025-01-04 17:57:32 +0400

    add capacitor to my project to make an application from it

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Easy Money',
+  webDir: 'dist'
+};
+
+export default config;
