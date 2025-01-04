e3c9a8b68e5b3396bbe9f38a2cd4cd1fbfb89c47
ElenMuradyan <muradyanelen0701@gmail.com>
2025-01-05 02:37:55 +0400

    modify MinActivity.java

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,20 +1,14 @@
 package com.example.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.community.admob.AdMob; 
-import com.getcapacitor.community.admob.AdMobConfig; 
+import androidx.appcompat.app.AppCompatActivity;  
+import com.getcapacitor.BridgeActivity; 
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends AppCompatActivity {  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        AdMobConfig config = new AdMobConfig();
-        config.setAppId("ca-app-pub-4052131431424102~5673792257");
-        AdMob.initialize(config);
+        setContentView(R.layout.activity_main); 
     }
 }
